46740bc5b844d5411a5ba8d4fe3ef02c8243ac07
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 01:56:10 +0300

    prepare database

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,16 +1,16 @@
 export const configuration = () => {
   return {
-    environment: process.env.ENV_NAME,
-    envName: process.env.ENV_NAME,
-    isProduction: process.env.IS_PRODUCTION,
-    baseUrl: process.env.BASE_URL,
+    environment: 'production',
+    envName: 'production',
+    isProduction: true,
+    baseUrl: 'process.env.BASE_URL',
     database: {
-      type: process.env.DATABASE_TYPE,
-      host: process.env.DATABASE_HOST,
-      port: process.env.DATABASE_PORT,
-      username: process.env.DATABASE_USERNAME,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_DATABASE,
+      type: 'process.env.DATABASE_TYPE',
+      host: 'process.env.DATABASE_HOST',
+      port: 'process.env.DATABASE_PORT',
+      username: 'process.env.DATABASE_USERNAME',
+      password: 'process.env.DATABASE_PASSWORD',
+      database: 'process.env.DATABASE_DATABASE',
     },
   };
 };
