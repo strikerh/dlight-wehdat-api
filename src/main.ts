063b6f36116e1c77f83c672c0d3e4fe64855b0b3
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 02:01:56 +0300

    prepare database

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const port = configService.get('PORT') || 3000;
-  await app.listen(port);
+  // await app.listen(port);
 }
 
 bootstrap();
