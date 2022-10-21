1ce142fa7d6aa4202acb71d47de41ceb334a7987
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 14:14:46 +0300

    prepare configurations

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,6 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3667);
+  await app.listen(3668);
 }
 bootstrap();
