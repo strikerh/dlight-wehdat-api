9d1ee23e1d6708256f50d54f579589694d646718
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 03:09:17 +0300

    prepare database

--- src/apartment/dto/create-apartment.dto.ts
+++ src/apartment/dto/create-apartment.dto.ts
@@ -3,19 +3,15 @@ import { IsArray, IsNotEmpty, IsNumber, Length } from 'class-validator';
 export class CreateApartmentDto {
   // id: string;
   @IsNotEmpty()
-  @Length(3, 255)
   name: string;
 
   price: string;
 
-  @IsNotEmpty()
   description: string;
 
-  @IsArray()
   images: string[];
   period: string;
 
-  @IsArray()
   features: { icon: string; text: string; suffix: string }[];
   address: string;
   contactNumber: string;
