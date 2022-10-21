53e8e8b77360693e49edf06139ec7d8bca956136
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 14:56:57 +0300

    prepare configurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -1,7 +1,7 @@
 export default () => ({
   port: process.env.APP_PORT,
   database: {
-    host: process.env.DATABASE_USER || 'sdfd',
-    port: 5432,
+    user: process.env.DATABASE_USER,
+    port: process.env.DATABASE_PORT,
   },
 });
