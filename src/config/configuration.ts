6eca7774b60924b4c0b1ab66cdfe0e00e2921dff
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 16:18:02 +0300

    prepare configurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -10,8 +10,8 @@ export interface Config {
 export default () => ({
   port: Number(process.env.APP_PORT) || 0,
   database: {
-    type: process.env.DATABASE_HOST || 'mysql',
-    host: process.env.DATABASE_TYPE || 'localhost',
+    type: process.env.DATABASE_TYPE || 'mysql',
+    host: process.env.DATABASE_HOST || 'localhost',
     port: process.env.DATABASE_PORT || 3306,
     username: process.env.DATABASE_USERNAME || 'root',
     password: process.env.DATABASE_PASSWORD.toString(),
