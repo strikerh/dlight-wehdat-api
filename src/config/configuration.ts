6ad27a08fbdec2aa74fb170e8318ff6a640fd797
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 16:16:26 +0300

    prepare configurations

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -14,7 +14,7 @@ export default () => ({
     host: process.env.DATABASE_TYPE || 'localhost',
     port: process.env.DATABASE_PORT || 3306,
     username: process.env.DATABASE_USERNAME || 'root',
-    password: process.env.DATABASE_PASSWORD.toString() === '' ? process.env.DATABASE_PASSWORD : '',
+    password: process.env.DATABASE_PASSWORD.toString(),
     database: process.env.DATABASE_DATABASE || 'dl_dlight',
     synchronize: process.env.DATABASE_SYNCHRONIZE || true,
     entities: [],
