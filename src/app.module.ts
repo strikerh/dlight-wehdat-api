d6dfdbec2edb4a2fb1ced647093f8495af870f59
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-21 14:35:34 +0300

    prepare configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import configuration from './config/configuration';
     ConfigModule.forRoot({
       isGlobal: true,
       load: [configuration],
-      envFilePath: ['.env', '.env.dev'],
+      envFilePath: ['.env.dev', '.env'],
     }),
   ],
   controllers: [AppController],
