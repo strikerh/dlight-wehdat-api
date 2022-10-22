71703809fd8816477a40ff458f63baf6d1141f2c
Hazem Safwat <HSafwat@moj.gov.sa>
2022-10-22 13:39:11 +0300

    test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { count } from "rxjs";
 
 @Injectable()
 export class AppService {
   constructor(private configService: ConfigService) {}
 
+  count = 0;
+
   getHello(): any {
+    this.count++;
     return {
       message: 'Server is Working!',
+      count: this.count,
       port: this.configService.get('port'),
       database: this.configService.get('database'),
     };
